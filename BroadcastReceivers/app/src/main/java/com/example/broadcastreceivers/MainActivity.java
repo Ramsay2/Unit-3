7400d9cb58b95057a4ae1b26cae97ea70572b3c4
Ramsay2 <sachin114sharma@gmail.com>
2021-07-29 16:15:24 +0530

    Broadcast receiver

---
+++ BroadcastReceivers/app/src/main/java/com/example/broadcastreceivers/MainActivity.java
@@ -0,0 +1,62 @@
+package com.example.broadcastreceivers;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.localbroadcastmanager.content.LocalBroadcastManager;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button mButton;
+    private TextView tvText;
+    private LocalBroadcastManager localBroadcastManager;
+    private LocalReceiver localReceiver;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        localBroadcastManager = LocalBroadcastManager.getInstance(this);
+        mButton = findViewById(R.id.btnSend);
+        tvText = findViewById(R.id.tvText);
+        registerLocal();
+        mButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent("com.broadcast.com");
+                intent.putExtra("key", "Hello Masai School");
+                localBroadcastManager.sendBroadcast(intent);
+            }
+        });
+    }
+
+    private void registerLocal() {
+        localReceiver = new LocalReceiver();
+        IntentFilter intentFilter = new IntentFilter("com.broadcast.com");
+        localBroadcastManager.registerReceiver(localReceiver, intentFilter);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        localBroadcastManager.unregisterReceiver(localReceiver);
+    }
+
+    private class LocalReceiver extends BroadcastReceiver {
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            if (intent != null) {
+               String data = intent.getStringExtra("key");
+               tvText.setText(data);
+            }
+        }
+    }
+}
