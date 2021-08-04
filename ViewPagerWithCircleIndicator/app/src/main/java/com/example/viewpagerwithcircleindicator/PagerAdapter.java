5fbbdecbb3686b8086fbb6ce07786abd330e77b9
Ramsay2 <sachin114sharma@gmail.com>
2021-08-04 19:03:34 +0530

    Pager Circular Indicator

---
+++ ViewPagerWithCircleIndicator/app/src/main/java/com/example/viewpagerwithcircleindicator/PagerAdapter.java
@@ -0,0 +1,34 @@
+package com.example.viewpagerwithcircleindicator;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.lifecycle.Lifecycle;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+
+public class PagerAdapter extends FragmentStateAdapter {
+
+    public PagerAdapter(@NonNull  FragmentManager fragmentManager, @NonNull Lifecycle lifecycle) {
+        super(fragmentManager, lifecycle);
+    }
+
+    @Override
+    public Fragment createFragment(int position) {
+        switch (position){
+            case 0:
+                return new FragmentA();
+            case 1:
+                return new FragmentB();
+            case 2:
+                return new FragmentC();
+            case 3:
+                return new FragmentD();
+        }
+        return null;
+    }
+
+    @Override
+    public int getItemCount() {
+        return 4;
+    }
+}
